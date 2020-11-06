549f22da4b9d9d04489d09e9bf8f16dee5e687f7
osakipokima <osakipokima@gmail.com>
2020-11-06 14:24:33 -0500

    Initalizing the Zookeeper project

---
+++ Problems/Reveal the hidden/main.py
@@ -0,0 +1,6 @@
+# the following line reads the input and converts it into a list; do not modify it, please
+hidden = list(input())
+
+# your code here
+a = len(hidden)
+print(a)
---
+++ Problems/Sum/main.py
@@ -0,0 +1,6 @@
+# put your python code here
+numInput1 = int(input())
+numInput2 = int(input())
+numInput3 = int(input())
+sumOfInput = numInput1 + numInput2 + numInput3
+print(sumOfInput)
